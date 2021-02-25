6f94d2c83836f541753a17f22d0069e462da8179
GuyMichaely <guymichaely@gmail.com>
2021-02-25 03:41:19 -0500

    Added variables to Game struct; moved around code to keep likes with likes

--- drawing.h
+++ drawing.h
@@ -1,5 +1,9 @@
 #include <ncurses.h>
 
+// calculate the onscreen coordinates of the line seperators
+// and store in calcLines
+// dimLength is the real length of the dimension (i.e. height or width)
+// dimUnits is the number of tiles in the dimension (e.g. dimUnits = 4 for a 4x4)
 void calcLines(int lineCoords[], int dimLength, int dimUnits) {
 	// store the onscreen coordinates of the dimUnits lines in lineCoords
 	
@@ -18,18 +22,23 @@ void calcLines(int lineCoords[], int dimLength, int dimUnits) {
 	}
 }
 
+// draw the tile seperation lines onscreen
 void initLines(Game *game) {
+	// horizontal lines
 	int rowLines[game->rows];
 	calcLines(rowLines, LINES, game->rows);
-	int colLines[game->cols];
-	calcLines(colLines, COLS, game->cols);
-
 	for (int y = 0; y < game->rows - 1; y++) {
 		mvhline(rowLines[y], 0, '-', COLS);
 	}
+
+	// vertical lines
+	int colLines[game->cols];
+	calcLines(colLines, COLS, game->cols);
 	for (int x = 0; x < game->cols; x++) {
 		mvvline(0, colLines[x], '|', LINES);
 	}
+
+	// intersections
 	for (int y = 0; y < game->rows - 1; y++) {
 		for (int x = 0; x < game->cols - 1; x++) {
 			mvprintw(rowLines[y], colLines[x], "+");
--- structs.h
+++ structs.h
@@ -1,5 +1,8 @@
 #pragma once
 typedef struct Game {
-	int rows;
-	int cols;
+	int rows; // rows in the game board
+	int cols; // columns in the game board
+	 // index (n - 1) tells you where n is
+	 // does not store index of empty tile
+	int tileIndices[];
 } Game;
